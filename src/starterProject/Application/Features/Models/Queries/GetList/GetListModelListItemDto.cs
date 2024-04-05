4ffae3f432ef34f30522f41a07f141769f20da4d
Sinan Ozturk <sinansinan_@outlook.com.tr>
2024-04-05 17:57:56 +0300

    Add project files.

---
+++ src/starterProject/Application/Features/Models/Queries/GetList/GetListModelListItemDto.cs
@@ -0,0 +1,12 @@
+using NArchitecture.Core.Application.Dtos;
+
+namespace Application.Features.Models.Queries.GetList;
+
+public class GetListModelListItemDto : IDto
+{
+    public Guid Id { get; set; }
+    public Guid BrandId { get; set; }
+    public string Name { get; set; }
+    public decimal DailyPrice { get; set; }
+    public string ImageUrl { get; set; }
+}
